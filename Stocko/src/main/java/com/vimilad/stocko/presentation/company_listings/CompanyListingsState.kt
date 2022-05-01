87255b4aebf38746ab75628708ee7d875b03e3f3
Milad Mohammadi <Mohammadi.Dev@gmail.com>
2022-05-01 14:17:41 +0430

    Implemented CompanyListingsViewModel

---
+++ Stocko/src/main/java/com/vimilad/stocko/presentation/company_listings/CompanyListingsState.kt
@@ -0,0 +1,10 @@
+package com.vimilad.stocko.presentation.company_listings
+
+import com.vimilad.stocko.domain.model.CompanyListing
+
+data class CompanyListingsState(
+    val companies: List<CompanyListing> = emptyList(),
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val searchQuery: String = ""
+)
